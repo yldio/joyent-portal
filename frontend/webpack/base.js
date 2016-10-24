73ab7705afd0a76037e4b3c9d047a3a9cb59ec66
Sérgio Ramos <mail@sergioramos.me>
2016-10-24 18:03:38 +0100

    fix "Unexpected token ,"

--- frontend/webpack/base.js
+++ frontend/webpack/base.js
@@ -19,7 +19,7 @@ const plugins = {
         ]
       }
     }
-  })
+  }),
   'define-plugin': new webpack.DefinePlugin({
     'process.env': {
       NODE_ENV: JSON.stringify(process.env['NODE_ENV'] || 'development'),
