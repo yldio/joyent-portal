0a08aa84dfb3101bacb16465410ee3f2ad4b5032
Alex Windett <windettalex@gmail.com>
2016-10-27 16:47:03 +0100

    REM Function - updating function to allow for multiple values to be pased in single arguement

--- ui/webpack/base.js
+++ ui/webpack/base.js
@@ -17,11 +17,16 @@ const plugins = {
           require('postcss-modules-values'),
           require('postcss-functions')({
             functions: {
-              remCalc: function(pixels) {
-                pixels = pixels.replace('px', '');
-                const baseValue = 16;
-                const remSize = pixels / baseValue;
-                return `${remSize}rem`;
+              remCalc: function(values) {
+                values = values.replace('px', '');
+                values = values.split(' ');
+                let outputRems = '';
+                const base = 16;
+                values.forEach( (value, i) => {
+                  const remValue = value / base;
+                  outputRems += i === 0 ? `${remValue}rem` : ` ${remValue}rem`;
+                });
+                return outputRems;
               }
             },
           }),
