08cecc66f796cf2a13388e5a03b9c6d82c85a797
Sérgio Ramos <mail@sergioramos.me>
2016-10-31 14:44:53 +0000

    bootstrap <Select> base component
    no actual implementation yet

---
+++ ui/src/components/select/index.js
@@ -0,0 +1,11 @@
+// const classNames = require('classnames');
+const React = require('react');
+// const styles = require('./style.css');
+
+const Select = () => {
+  return (<div />);
+};
+
+Select.propTypes = {};
+
+module.exports = Select;
