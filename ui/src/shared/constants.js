f66b9a03af027251a497c145ccec2f6a1b9aa7c0
Alex Windett <windettalex@gmail.com>
2016-10-31 12:25:20 +0000

    FIX - missing ","

--- ui/src/shared/constants.js
+++ ui/src/shared/constants.js
@@ -69,7 +69,7 @@ const sizes = {
 const boxes = {
   borderRadius: 4,
   bottomShaddow: '0 2px 0 0 rgba(0, 0, 0, 0.05)',
-  insetShaddow: 'inset 0 3px 0 0 rgba(0, 0, 0, 0.05)'
+  insetShaddow: 'inset 0 3px 0 0 rgba(0, 0, 0, 0.05)',
   border: {
     checked: '1px solid #2532bb',
     unchecked: '1px solid #d8d8d8',
