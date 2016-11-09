0a1f8d2f66c8643218a9dcfbca4a3361e2b180fa
Sérgio Ramos <mail@sergioramos.me>
2016-11-09 13:28:26 +0000

    epoch matrix implementation

---
+++ spikes/graphs-matrix/epoch/server/routes/static.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = (server) => {
+  // server.route({
+  //   method: 'GET',
+  //   path: '/{param*}',
+  //   handler: {
+  //     directory: {
+  //       path: path.join(__dirname, '../../static'),
+  //       redirectToSlash: true,
+  //       index: true
+  //     }
+  //   }
+  // });
+};
