2b7e31a6c8cf85e6fcbadd40aef09ebf55de887d
Sérgio Ramos <mail@sergioramos.me>
2016-12-15 14:09:23 +0000

    implement projects component skeleton
    for both orgs and users

---
+++ frontend/src/components/projects/index.js
@@ -0,0 +1,68 @@
+const React = require('react');
+const ReactIntl = require('react-intl');
+// const ReactRouter = require('react-router');
+
+const Column = require('@ui/components/column');
+const Button = require('@ui/components/button');
+const Row = require('@ui/components/row');
+
+const {
+  FormattedMessage
+} = ReactIntl;
+
+// const {
+//   Link,
+//   Match,
+//   Miss,
+//   Redirect
+// } = ReactRouter;
+
+const Projects = ({
+  projects = []
+}) => {
+  console.log(projects);
+  const empty = projects.length ? null : (
+    <Row>
+      <Column xs={12}>
+        <p>
+          <FormattedMessage id='no-personal-projects' />
+        </p>
+      </Column>
+    </Row>
+  );
+
+  const _projects = projects.map((project) => (
+    <li key={project.id}>
+      <input type='checkbox' />
+      <span>{project.name} ({project.plan}) ⚙️</span>
+    </li>
+  ));
+
+  return (
+    <div>
+      {empty}
+      <Row>
+        <Column xs={12}>
+          <Button>
+            <FormattedMessage id='create-new' />
+          </Button>
+        </Column>
+      </Row>
+      <Row>
+        <ul>
+          {_projects}
+        </ul>
+      </Row>
+    </div>
+  );
+};
+
+Projects.propTypes = {
+  projects: React.PropTypes.arrayOf(React.PropTypes.shape({
+    id: React.PropTypes.string,
+    name: React.PropTypes.string,
+    plan: React.PropTypes.string
+  }))
+};
+
+module.exports = Projects;
