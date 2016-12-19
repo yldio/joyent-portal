4bd334cb311fd092fea7bcf76cfc54e6fe5849fe
Sérgio Ramos <mail@sergioramos.me>
2016-12-19 12:24:00 +0000

    local node_modules as first resolve path

--- frontend/webpack/paths.js
+++ frontend/webpack/paths.js
@@ -7,9 +7,9 @@ const FRONTEND = path.join(ROOT, 'frontend');
 module.exports = {
   ROOT,
   MODULES: [
+    'node_modules',
     path.join(FRONTEND, 'node_modules'),
     path.join(UI, 'node_modules'),
-    'node_modules'
   ],
   FRONTEND: path.join(FRONTEND, 'src'),
   UI: path.join(UI, 'src'),
