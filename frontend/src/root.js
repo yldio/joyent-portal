26991ab356a5c41e98722931fc5c9341a0aaa27a
Sérgio Ramos <mail@sergioramos.me>
2016-12-19 12:25:37 +0000

    use json mocked state

--- frontend/src/root.js
+++ frontend/src/root.js
@@ -23,61 +23,7 @@ const {
   BrowserRouter
 } = ReactRouter;
 
-const store = Store({
-  projects: {
-    data: [{
-      uuid: 'e0ea0c02-55cc-45fe-8064-3e5176a59401',
-      org: 'e12ad7db-91b2-4154-83dd-40dcfc700dcc',
-      id: 'forest-foundation-dev',
-      name: 'Forest Foundation Dev',
-      plan: '20.05$ per day'
-    }, {
-      uuid: '9fcb374d-a267-4c2a-9d9c-ba469b804639',
-      org: 'e12ad7db-91b2-4154-83dd-40dcfc700dcc',
-      id: 'forest-foundation-testing',
-      name: 'Forest Foundation Testing',
-      plan: '20.05$ per day'
-    }, {
-      uuid: 'ac2c2498-e865-4ee3-9e26-8c75a81cbe25',
-      org: 'e12ad7db-91b2-4154-83dd-40dcfc700dcc',
-      id: 'forest-foundation-production',
-      name: 'Forest Foundation Production',
-      plan: '100.17$ per day'
-    }]
-  },
-  orgs: {
-    ui: {
-      sections: [
-        'projects',
-        'people',
-        'settings'
-      ]
-    },
-    data: [{
-      hide: ['people'],
-      owner: 'b94033c1-3665-4c36-afab-d9c3d0b51c01',
-      id: 'nicola',
-      name: 'Your Dashboard'
-    }, {
-      owner: 'b94033c1-3665-4c36-afab-d9c3d0b51c01',
-      uuid: 'e12ad7db-91b2-4154-83dd-40dcfc700dcc',
-      id: 'biz-tech',
-      name: 'BizTech'
-    }, {
-      owner: 'b94033c1-3665-4c36-afab-d9c3d0b51c01',
-      uuid: '551f316d-e414-480f-9787-b4c408db3edd',
-      id: 'make-us-proud',
-      name: 'Make Us Proud',
-    }]
-  },
-  account: {
-    data: {
-      uuid: 'b94033c1-3665-4c36-afab-d9c3d0b51c01',
-      id: 'nicola',
-      name: 'Nicola'
-    }
-  }
-});
+const store = Store(require('./mock-state.json'));
 
 module.exports = () => {
   return (
