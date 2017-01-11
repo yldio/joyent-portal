466fe72459542acc54e6e6fcd777df24d73614c2
Alex Windett <windettalex@gmail.com>
2017-01-11 14:08:47 +0000

    sorting stories alphabetically

--- ui/.storybook/config.js
+++ ui/.storybook/config.js
@@ -3,7 +3,13 @@ const { configure } = require('@kadira/storybook');
 const req = require.context('../src/components', true, /story.js$/)
 
 function loadStories() {
-  req.keys().forEach((filename) => req(filename));
+  let stories = req.keys();
+      stories = stories.sort();
+
+  stories.forEach( story => req(story));
+
+  // Fallback to stories/index.js file for anything that
+  // hasn't been moved
   require('../stories');
 }
 
