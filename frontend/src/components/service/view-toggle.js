f2bdd00a3245052bc7b317ca881749993a5ab88b
Alex Windett <windettalex@gmail.com>
2017-02-07 17:27:19 +0000

    styling positon of service toggle

--- frontend/src/components/service/view-toggle.js
+++ frontend/src/components/service/view-toggle.js
@@ -1,13 +1,34 @@
 const React = require('react');
 const Toggle = require('@ui/components/toggle');
 
+const fns = require('@ui/shared/functions');
+
+const Styled = require('styled-components');
+
+const {
+  remcalc,
+} = fns;
+
+const {
+  default: styled,
+} = Styled;
+
+const StyledWrapper = styled.div`
+  margin-top: ${remcalc(9)};
+  margin-bottom: ${remcalc(36)};
+`;
+
+const StyledSpan = styled.span`
+  margin-right: ${remcalc(6)};
+`;
+
 const ServiceToggle = () => {
 
   return (
-    <div>
-      <span>View</span>
+    <StyledWrapper>
+      <StyledSpan>View</StyledSpan>
       <Toggle />
-    </div>
+    </StyledWrapper>
   );
 };
 
