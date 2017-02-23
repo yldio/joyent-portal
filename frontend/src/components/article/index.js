fbff74a13f0e39517aa984d99fd3def31e3c9545
JUDIT GRESKOVITS <juditgreskovits@gmail.com>
2017-02-23 16:39:06 +0000

    Fix background colour

--- frontend/src/components/article/index.js
+++ frontend/src/components/article/index.js
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
 import { colors, breakpoints } from '@ui/shared/constants';
+import { unitcalc } from '@ui/shared/functions';
 
 // Main Contonent Wrapper Styles
 export default styled.article`
-  background-color: ${colors.base.grey};
-  padding: 2rem;
+  background-color: ${colors.base.background};
+  padding: ${unitcalc(1)};
 
   ${breakpoints.large`
     padding: 0;
