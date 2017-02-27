961a06f46a03aaafb04fcb8e989857655c5e220e
Alex Windett <windettalex@gmail.com>
2017-02-27 16:56:23 +0000

    removing top border of main article wrapper

--- frontend/src/components/article/index.js
+++ frontend/src/components/article/index.js
@@ -2,10 +2,10 @@ import styled from 'styled-components';
 import { breakpoints, colors } from '@ui/shared/constants';
 import { remcalc } from '@ui/shared/functions';
 
-// Main Contonent Wrapper Styles
+// Main content Wrapper Styles
 export default styled.article`
   padding: 2rem;
-  border-top: ${remcalc(1)} solid ${colors.base.grey};
+  // border-top: ${remcalc(1)} solid ${colors.base.grey};
 
   ${breakpoints.large`
     padding: 0;
