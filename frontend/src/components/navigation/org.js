3b1e116453521918a5a8ca73e6e016520bcd209a
Alex Windett <windettalex@gmail.com>
2017-03-02 11:15:33 +0000

    changing value for coditionally assigning org nav margin

--- frontend/src/components/navigation/org.js
+++ frontend/src/components/navigation/org.js
@@ -45,7 +45,7 @@ const OrgAvatar = styled(Avatar)`
 `;
 
 const OrgName = styled.span`
-  margin-left: ${props => props.avatar ? remcalc(12) : 0};
+  margin-left: ${props => props.hasAvatar ? remcalc(12) : 0};
   margin-top: ${remcalc(3)};
   color: ${colors.base.text}
 `;
@@ -117,7 +117,7 @@ const OrgNavigation = ({
               width={remcalc(26)}
             />
           </OrgImage>
-          <OrgName avatar={image}>
+          <OrgName hasAvatar>
             {name}
           </OrgName>
         </NavigationLinkContainer>
