7da2460b216abea6b70170cb14f2749c5424f4af
Sérgio Ramos <mail@sergioramos.me>
2017-05-25 19:45:57 +0100

    chore: allow packages to opt-out of prettier

--- scripts/prettier.js
+++ scripts/prettier.js
@@ -1,13 +1,25 @@
 #!/usr/bin/env node
 
+const { config } = require('../package.json');
+const forceArray = require('force-array');
 const execa = require('execa');
 const globby = require('globby');
 const main = require('apr-main');
 const argv = require('yargs').argv;
 const path = require('path');
 
+const optOut = forceArray(config['fmt-opt-out']);
+
+const filter = (files = []) =>
+  files
+    .filter(file => !/node_modules/.test(file))
+    .filter(file => !optOut.some(pkg => file.indexOf(`packages/${pkg}`) === 0));
+
 const run = async (files = []) => {
-  const cp = execa('prettier', ['--write', '--single-quote'].concat(files));
+  const cp = execa(
+    'prettier',
+    ['--write', '--single-quote'].concat(filter(files))
+  );
 
   cp.stdout.pipe(process.stdout);
   cp.stderr.pipe(process.stderr);
@@ -20,7 +32,7 @@ const all = async () => {
     cwd: path.join(__dirname, '..')
   });
 
-  return run(files.filter(pathname => !/node_modules/.test(pathname)));
+  return run(files);
 };
 
 main(argv._.length ? run(argv._) : all());
