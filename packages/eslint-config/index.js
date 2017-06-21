aa8d9f7a25c74756ced48229b430c4d9417a9744
Sérgio Ramos <mail@sergioramos.me>
2017-06-21 17:48:15 +0100

    feat(eslint-config-joyent-portal): don't lint anything prettier related

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -2,22 +2,8 @@
 // TODO wait for eslint/eslint#3458
 
 module.exports = {
-  extends: ['eslint:recommended', 'xo-space/esnext', 'react-app', 'prettier'],
-  plugins: ['prettier'],
+  extends: ['eslint:recommended', 'xo-space/esnext', 'react-app', 'prettier', 'prettier/react'],
   rules: {
-    'capitalized-comments': 0,
-    'prettier/prettier': [
-      'error',
-      {
-        useTabs: false,
-        printWidth: 80,
-        tabWidth: 2,
-        singleQuote: true,
-        trailingComma: 'none',
-        bracketSpacing: true,
-        jsxBracketSameLine: false,
-        semi: true
-      }
-    ]
+    'capitalized-comments': 0
   }
 };
