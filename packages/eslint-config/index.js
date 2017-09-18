7749f1eb62805785615534d3d8c4c9ff3e1b8abc
Sérgio Ramos <mail@sergioramos.me>
2017-09-18 14:05:45 +0100

    chore: remove extracted packages

--- packages/eslint-config/index.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line no-warning-comments
-// TODO wait for eslint/eslint#3458
-
-module.exports = {
-  extends: ['eslint:recommended', 'xo-space/esnext', 'react-app', 'prettier', 'prettier/react'],
-  rules: {
-    'capitalized-comments': 0
-  }
-};
--- packages/manifest-editor/test/index.js
+++
@@ -1,8 +0,0 @@
-// import React from 'react';
-// import renderer from 'react-test-renderer';
-// import ManifestEditor from '../src/index';
-
-// It('renders <ManifestEditor /> correctly', () => {
-//   const tree = renderer.create(<ManifestEditor />).toJSON();
-//   expect(tree).toMatchSnapshot();
-// });
