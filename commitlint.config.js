549e08717a53b18b0e4cde578e6a057b32095b02
Sérgio Ramos <mail@sergioramos.me>
2017-10-12 19:52:11 +0100

    ci: don't run tests in pre-commit

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  extends: ['@commitlint/config-angular', '@commitlint/config-lerna-scopes']
+  extends: ['@commitlint/config-angular'],
+  rules: {
+    'scope-enum': [
+      2,
+      'always',
+      ['ui-toolkit', 'my-joyent', 'my-joy-beta', 'boilerplate', 'cloudapi-gql']
+    ]
+  }
 };
