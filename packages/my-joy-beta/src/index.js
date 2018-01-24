0681fb05fecfdad1b4aea236a46a96b81bfdaecf
Sérgio Ramos <mail@sergioramos.me>
2018-01-24 11:35:06 +0000

    fix(my-joy-beta): polyfill Number.isFinite

--- packages/my-joy-beta/src/index.js
+++ packages/my-joy-beta/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import isFunction from 'lodash.isfunction';
+import isFinite from 'lodash.isfinite';
 
 import { register } from './sw';
 import App from './app';
+
+if (!isFunction(Number.isFinite)) {
+  Number.isFinite = isFinite;
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
