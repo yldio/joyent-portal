2e36b850caffb3614b362f145d699e6730287d81
Sara Vieira <hey@iamsaravieira.com>
2018-01-30 18:58:40 +0100

    fix(my-joy-beta): fix footer position
    fixes #1045

--- packages/ui-toolkit/src/layout/page-container.js
+++ packages/ui-toolkit/src/layout/page-container.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
+import remcalc from 'remcalc';
 
 export default styled.div`
   display: flex;
   flex: 1 1 auto;
   position: relative;
   flex-flow: column;
+  padding-bottom: ${remcalc(60)};
 `;
