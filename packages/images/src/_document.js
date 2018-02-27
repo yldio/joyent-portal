e540425472252eff499b7d1b3a5acef124007f90
Sérgio Ramos <mail@sergioramos.me>
2018-02-27 10:59:15 +0000

    fix: require theme from toolkit, not state

--- packages/images/src/_document.js
+++ packages/images/src/_document.js
@@ -3,7 +3,7 @@ const Document = require('hapi-render-react-joyent-document');
 const path = require('path');
 const url = require('url');
 
-const { default: theme } = require('./state/theme');
+const { theme } = require('joyent-ui-toolkit');
 const { default: createClient } = require('./state/apollo-client');
 const { default: createStore } = require('./state/redux-store');
 
