110c80f65f2e3ef316e82957b7bbac4f09911572
Sérgio Ramos <mail@sergioramos.me>
2018-03-02 11:52:28 +0000

    chore(instances): remove resize file

--- packages/my-joy-instances/src/components/instances/index.js
+++ packages/my-joy-instances/src/components/instances/index.js
@@ -1,3 +1,2 @@
 export { default as List } from './list';
-export { default as Resize } from './resize';
 export { default as Snapshots } from './snapshots';
--- packages/my-joy-instances/src/components/instances/resize.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default ({ instance, packages, handleSubmit }) => (
-  <form onSubmit={handleSubmit}>
-    <pre>{JSON.stringify({ instance, packages }, null, 2)}</pre>;
-  </form>
-);
